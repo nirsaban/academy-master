b38644a0c83b8f5e2f92d7e2799b4125a2678785
nirsa <nirsa11@gmail.com>
2020-11-07 17:55:12 +0200

    wip

--- resources/views/masters/studentMaster.blade.php
+++ resources/views/masters/studentMaster.blade.php
@@ -29,7 +29,7 @@
             <div class="container-fluid">
                 <div class="navbar-wrapper">
                     <div class="logo">
-                        <div class="d-flex justify-content-center">
+                        <div class="">
                         <img  onclick="window.location='{{url('/student')}}'"
                                 class="img-fluid"
                                 src="{{asset('images/logo.jpeg')}}"
