d1b738199994b15ac2bede5404f6e7567b80967a
nirsa <nirsa11@gmail.com>
2020-12-25 13:03:09 +0200

    wip

--- resources/views/student/partials/works.blade.php
+++ resources/views/student/partials/works.blade.php
@@ -59,6 +59,7 @@
                          </select>
                       </div>
                   </div>
+                  <i class="fa fa-trash-alt fa-md fa-2x" style="color: rgb(238, 126, 126);margin-left:2px" onclick="this.parentNode.remove()"></i>
                 @endif
 
             </div>
