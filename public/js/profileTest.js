7ba7fce3b4600fe55410dc225a6d59f3894e5ac8
nirsa <nirsa11@gmail.com>
2020-12-25 13:15:58 +0200

    wip

--- public/js/profileTest.js
+++ public/js/profileTest.js
@@ -69,7 +69,7 @@ function updateEducation(id) {
         eduObj['endYear'] = endYear
         educationArr.push(eduObj)
     })
-    let allStart = document.querySelectorAll('.start');
+    let allStart = document.querySelectorAll('#myModalEducation > .start');
     allStart.forEach(start => {
         console.log(start.value)
         if (start.style.border == '1px solid red' || start.value == '') {
@@ -130,7 +130,7 @@ function updateWorks(id) {
         workObj['endYear'] = endYear
         worksArr.push(workObj)
     })
-    let allStart = document.querySelectorAll('.start');
+    let allStart = document.querySelectorAll('#myModalWorks > .start');
     allStart.forEach(start => {
         if (start.style.border == '1px solid red' || start.value == '') {
             document.getElementById('errorWork').textContent = 'you have an error in your date inputs'
